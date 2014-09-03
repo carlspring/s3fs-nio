f659d9fc878e58f686f5f1c1dba5f97aa8b3e4c7
Javier Arnáiz <arnaix@gmail.com>
2014-09-03 13:56:34 +0200

    temporal fix for bug #20

--- src/main/java/com/upplication/s3fs/S3FileSystemProvider.java
+++ src/main/java/com/upplication/s3fs/S3FileSystemProvider.java
@@ -6,8 +6,8 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
+import com.upplication.s3fs.util.FileTypeDetector;
 import com.upplication.s3fs.util.IOUtils;
-import org.apache.tika.Tika;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -61,6 +61,8 @@ public class S3FileSystemProvider extends FileSystemProvider {
 	public static final String SECRET_KEY = "secret_key";
 
 	final AtomicReference<S3FileSystem> fileSystem = new AtomicReference<>();
+
+    private final FileTypeDetector fileTypeDetector = new com.upplication.s3fs.util.FileTypeDetector();
 
 	@Override
 	public String getScheme() {
@@ -228,7 +230,8 @@ public class S3FileSystemProvider extends FileSystemProvider {
                 if (Files.exists(tempFile)){
                     ObjectMetadata metadata = new ObjectMetadata();
                     metadata.setContentLength(Files.size(tempFile));
-                    metadata.setContentType(Files.probeContentType(tempFile));
+                    // FIXME: #20 ServiceLoader cant load com.upplication.s3fs.util.FileTypeDetector when this library is used inside a ear :(
+                    metadata.setContentType(fileTypeDetector.probeContentType(tempFile));
                     try (InputStream stream = Files.newInputStream(tempFile)){
                         /*
                          FIXME: if the stream is {@link InputStream#markSupported()} i can reuse the same stream
