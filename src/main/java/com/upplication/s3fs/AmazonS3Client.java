3c85b4acadbb30279c5f97c08cd3e0bbe8f0c0f6
sbeimin <sbeimin@betterbe.com>
2014-11-19 16:50:57 +0100

    Added more delegate functions for bucket operations (exists / create)

--- src/main/java/com/upplication/s3fs/AmazonS3Client.java
+++ src/main/java/com/upplication/s3fs/AmazonS3Client.java
@@ -1,11 +1,24 @@
 package com.upplication.s3fs;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.*;
-
 import java.io.File;
 import java.io.InputStream;
 import java.util.List;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AccessControlList;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.CopyObjectResult;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.GetBucketAclRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.Owner;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.Region;
+import com.amazonaws.services.s3.model.S3Object;
 /**
  * Client Amazon S3
  * @see com.amazonaws.services.s3.AmazonS3Client
@@ -17,30 +30,35 @@ public class AmazonS3Client {
 	public AmazonS3Client(AmazonS3 client){
 		this.client = client;
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#listBuckets()
 	 */
 	public List<Bucket> listBuckets() {
 		return client.listBuckets();
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#listObjects(ListObjectsRequest)
 	 */
 	public ObjectListing listObjects(ListObjectsRequest request) {
 		return client.listObjects(request);
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#getObject(String, String)
 	 */
 	public S3Object getObject(String bucketName, String key) {
 		return client.getObject(bucketName, key);
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#putObject(String, String, File)
 	 */
 	public PutObjectResult putObject(String bucket, String key, File file) {
 		return client.putObject(bucket, key, file);
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#putObject(String, String, java.io.InputStream, ObjectMetadata)
 	 */
@@ -48,12 +66,14 @@ public class AmazonS3Client {
 			InputStream inputStream, ObjectMetadata metadata) {
 		return client.putObject(bucket, keyName, inputStream, metadata);
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#deleteObject(String, String)
 	 */
 	public void deleteObject(String bucket, String key) {
 		client.deleteObject(bucket, key);
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#copyObject(String, String, String, String)
 	 */
@@ -61,30 +81,35 @@ public class AmazonS3Client {
 			String destinationKey) {
 		return client.copyObject(sourceBucketName, sourceKey, destinationBucketName, destinationKey);
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#getBucketAcl(String)
 	 */
 	public AccessControlList getBucketAcl(String bucket) {
 		return client.getBucketAcl(bucket);
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#getS3AccountOwner()
 	 */
 	public Owner getS3AccountOwner() {
 		return client.getS3AccountOwner();
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#setEndpoint(String)
 	 */
 	public void setEndpoint(String endpoint) {
 		client.setEndpoint(endpoint);
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#getObjectAcl(String, String)
 	 */
 	public AccessControlList getObjectAcl(String bucketName, String key) {
 		return client.getObjectAcl(bucketName, key);
 	}
+	
 	/**
 	 * @see com.amazonaws.services.s3.AmazonS3Client#getObjectMetadata(String, String)
 	 */
@@ -98,4 +123,72 @@ public class AmazonS3Client {
     public ObjectListing listNextBatchOfObjects(ObjectListing objectListing) {
         return client.listNextBatchOfObjects(objectListing);
     }
+
+	/**
+	 * @param bucketName
+	 * @return
+	 * @throws AmazonClientException
+	 * @throws AmazonServiceException
+	 * @see com.amazonaws.services.s3.AmazonS3#doesBucketExist(java.lang.String)
+	 */
+	public boolean doesBucketExist(String bucketName) throws AmazonClientException, AmazonServiceException {
+		return client.doesBucketExist(bucketName);
+	}
+
+	/**
+	 * @param createBucketRequest
+	 * @return
+	 * @throws AmazonClientException
+	 * @throws AmazonServiceException
+	 * @see com.amazonaws.services.s3.AmazonS3#createBucket(com.amazonaws.services.s3.model.CreateBucketRequest)
+	 */
+	public Bucket createBucket(CreateBucketRequest createBucketRequest) throws AmazonClientException, AmazonServiceException {
+		return client.createBucket(createBucketRequest);
+	}
+
+	/**
+	 * @param bucketName
+	 * @return
+	 * @throws AmazonClientException
+	 * @throws AmazonServiceException
+	 * @see com.amazonaws.services.s3.AmazonS3#createBucket(java.lang.String)
+	 */
+	public Bucket createBucket(String bucketName) throws AmazonClientException, AmazonServiceException {
+		return client.createBucket(bucketName);
+	}
+
+	/**
+	 * @param bucketName
+	 * @param region
+	 * @return
+	 * @throws AmazonClientException
+	 * @throws AmazonServiceException
+	 * @see com.amazonaws.services.s3.AmazonS3#createBucket(java.lang.String, com.amazonaws.services.s3.model.Region)
+	 */
+	public Bucket createBucket(String bucketName, Region region) throws AmazonClientException, AmazonServiceException {
+		return client.createBucket(bucketName, region);
+	}
+
+	/**
+	 * @param bucketName
+	 * @param region
+	 * @return
+	 * @throws AmazonClientException
+	 * @throws AmazonServiceException
+	 * @see com.amazonaws.services.s3.AmazonS3#createBucket(java.lang.String, java.lang.String)
+	 */
+	public Bucket createBucket(String bucketName, String region) throws AmazonClientException, AmazonServiceException {
+		return client.createBucket(bucketName, region);
+	}
+
+	/**
+	 * @param getBucketAclRequest
+	 * @return
+	 * @throws AmazonClientException
+	 * @throws AmazonServiceException
+	 * @see com.amazonaws.services.s3.AmazonS3#getBucketAcl(com.amazonaws.services.s3.model.GetBucketAclRequest)
+	 */
+	public AccessControlList getBucketAcl(GetBucketAclRequest getBucketAclRequest) throws AmazonClientException, AmazonServiceException {
+		return client.getBucketAcl(getBucketAclRequest);
+	}
 }
