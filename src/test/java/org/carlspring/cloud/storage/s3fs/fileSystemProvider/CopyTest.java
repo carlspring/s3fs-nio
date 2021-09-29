498e809c4c00939ca89421f6eb82c64a15782093
Enno Ruijters <eruijters@betterbe.com>
2021-09-29 11:00:34 +0200

    Add unit test

--- src/test/java/org/carlspring/cloud/storage/s3fs/fileSystemProvider/CopyTest.java
+++ src/test/java/org/carlspring/cloud/storage/s3fs/fileSystemProvider/CopyTest.java
@@ -19,6 +19,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -124,6 +125,37 @@ class CopyTest
         Exception exception = assertThrows(FileAlreadyExistsException.class, () -> s3fsProvider.copy(file, fileDest));
 
         assertNotNull(exception);
+    }
+
+    @Test
+    public void copyDirectory() throws IOException
+    {
+        final String content = "sample-content";
+
+        // fixtures
+        S3ClientMock client = S3MockFactory.getS3ClientMock();
+        client.bucket("bucketA").dir("dir1").file("dir1/file", content.getBytes());
+        FileSystem fs = createNewS3FileSystem();
+        Path dir1 = fs.getPath("/bucketA", "dir1");
+        Path file1 = fs.getPath("/bucketA", "dir1", "file");
+        Path dir2 = fs.getPath("/bucketA", "dir2");
+        Path file2 = fs.getPath("/bucketA", "dir2", "file");
+
+        // assert
+        assertTrue(Files.exists(dir1));
+        assertTrue(Files.exists(file1));
+        assertTrue(Files.isDirectory(dir1));
+        assertTrue(Files.isRegularFile(file1));
+        assertFalse(Files.exists(dir2));
+        assertFalse(Files.exists(file2));
+
+        // act
+        s3fsProvider.copy(dir1, dir2);
+
+        // assertions
+        assertTrue(Files.exists(dir2));
+        assertTrue(Files.isDirectory(dir2));
+        assertFalse(Files.exists(file2));
     }
 
     /**
