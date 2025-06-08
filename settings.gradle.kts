048b07044e01cfd61dc03940098ad5f0980d16d9
Steve Todorov <steve.todorov@carlspring.com>
2025-06-08 15:56:28 +0300

    build: Upgrade from deprecated `com.gradle.enterprise` to `com.gradle.develocity` (#902)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,18 +13,44 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version("3.13")
+    id("com.gradle.develocity") version "3.19.2"
 }
 
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+
+        // Automatically publish scans in CI
+        publishing.onlyIf { !System.getenv("CI").isNullOrEmpty() }
+
         capture {
-            // Capture task inputs.
-            isTaskInputFiles = true
+            // https://docs.gradle.com/enterprise/gradle-plugin/current/#capturing_resource_usage
+            resourceUsage.set(true)
+            buildLogging.set(false)
+            testLogging.set(false)
         }
-        // "auto-accept" license agreements only when running under CI server.
-        publishAlwaysIf(!System.getenv("CI").isNullOrEmpty())
+
+        obfuscation {
+            username { _ -> "__redacted__" }
+            ipAddresses { addresses -> addresses.map { _ -> "0.0.0.0" } }
+            externalProcessName { processName -> "__redacted__" }
+        }
+
+        // Add custom values
+        value("Project Name", rootProject.name)
+        value("Build Branch", System.getenv("GITHUB_REF")?.replace("refs/heads/", "") ?: "N/A")
+        value("Build Commit", System.getenv("GITHUB_SHA") ?: "N/A")
+
+        val ciLink = System.getenv("GITHUB_ACTIONS")?.let { "https://github.com/${System.getenv("GITHUB_REPOSITORY")}/actions/runs/${System.getenv("GITHUB_RUN_ID")}" } ?: "N/A"
+        // Add tags to the build scan
+        tag("CI")
+        tag(System.getProperty("os.name"))
+        // Add links to related information
+        if(ciLink != "N/A") {
+            link("GitHub Action", ciLink)
+        } else {
+            tag("local dev build")
+        }
     }
 }
